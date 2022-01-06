3ee2b5e0da27a32e7691597c8d9a2c24230d62ea
Dahyeon Kye <rpekgus@gmail.com>
2022-01-06 14:27:40 +0900

    BOJ / 회의실 배정 / Python

---
+++ kyekye/01Greedy/[BOJ] 1931.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+times = []
+
+for _ in range(n):
+    time = list(map(int, input().split()))
+    times.append(time)
+
+times.sort()
+times.sort(key=lambda a: a[1])
+
+end = times[0][1]
+ans = 1
+for i in range(1, len(times)):
+    if times[i][0] >= end:
+        ans += 1
+        end = times[i][1]
+
+print(ans)
