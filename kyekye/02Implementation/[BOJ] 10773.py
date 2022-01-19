03efc1fe164c56d325faa676251714359ff6715a
Dahyeon Kye <rpekgus@gmail.com>
2022-01-19 23:09:37 +0900

    BOJ / 제로

---
+++ kyekye/02Implementation/[BOJ] 10773.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+nums = []
+
+for _ in (range(int(input()))):
+    num = int(input())
+    if num == 0:
+        nums.pop()
+    else:
+        nums.append(num)
+
+print(sum(nums))
