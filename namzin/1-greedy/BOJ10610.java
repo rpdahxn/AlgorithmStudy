49e3535152665482b281be730f20b66d9f3882b8
pongonam <89187236+pongonam@users.noreply.github.com>
2022-01-20 18:34:15 +0900

    test git desktop

---
+++ namzin/1-greedy/BOJ10610.java
@@ -0,0 +1,7 @@
+public class BOJ10610 {
+    public static void main(String[] args) {
+        System.out.println("10610...");
+        System.out.println("...");
+        System.out.println("z");
+    }
+}
