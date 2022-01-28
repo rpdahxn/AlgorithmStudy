87b0bbe6f984f7e5cd53adccc2594b9a5385ded5
Dahyeon Kye <rpekgus@gmail.com>
2022-01-28 15:32:22 +0900

    BOJ / 11866 / 요세푸스 문제 0

--- kyekye/02Implementation/[BOJ] 11866.py
+++ kyekye/02Implementation/[BOJ] 11866.py
@@ -1,14 +1,29 @@
+# import sys
+# input = sys.stdin.readline
+
+# n, k = map(int, input().split())
+
+# people = list(range(1, n+1))
+# ans = []
+# idx = 0
+
+# while people:
+#     idx = (idx + k - 1) % len(people)
+#     ans.append(str(people.pop(idx)))
+
+# print("<" + ", ".join(ans) + ">")
+
 import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
 
-people = list(range(1, n+1))
-ans = []
 idx = 0
+people = [i+1 for i in range(n)]
+ans = []
 
 while people:
-    idx = (idx + k - 1) % len(people)
+    idx  = (idx + k - 1) % len(people)
     ans.append(str(people.pop(idx)))
-
-print("<" + ", ".join(ans) + ">")
+    
+print("<" + ", ".join(ans)+ ">")
