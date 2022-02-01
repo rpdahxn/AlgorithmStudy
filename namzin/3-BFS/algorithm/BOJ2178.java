240c07353b6a3af6c543ea64b343fddba2074e21
nampongo <96946588+nampongo@users.noreply.github.com>
2022-02-01 22:50:31 +0900

    220201-TUE

--- namzin/3-BFS/algorithm/BOJ2178.java
+++ namzin/3-BFS/algorithm/BOJ2178.java
@@ -1,52 +1,54 @@
 package algorithm;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-//bfs·Î Ç®±â
 public class BOJ2178 {
-	public static StringBuilder sb;
-	public static boolean[][] map;
-	public static boolean[][] visited;
-	public static int mindep, n, m;
+	static int[][] map;
+	static int n, m;
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		sb = new StringBuilder();
 		
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		map = new boolean[n+1][m+1];
-		visited = new boolean[n+1][m+1];
-		mindep = Integer.MAX_VALUE;
+		map = new int[n][m];
 		
-		for(int i=1; i<=n; i++) {
+		for(int i=0; i<n; i++) {
 			String str = br.readLine();
-			for(int j=1, cnt=0; j<=m; j++) 
-				if(str.substring(cnt, ++cnt).equals("1")) map[i][j] = true;
+			for(int j=0; j<m; j++) map[i][j] = str.charAt(j)-'0';
 		}
 		
-		dfs(1, 1, 1);
-		
-		sb.append(mindep);
-		bw.write(sb.toString());
-		br.close();
-		
-		bw.flush();
-		bw.close();
+		bfs();
+		System.out.println(map[n-1][m-1]);
 	}
 	
-	public static void dfs(int x, int y, int depth) {
-		if(x==n && y==m && mindep > depth) mindep = depth;
-		if(depth >= mindep) return;
+	public static void bfs() {
+		int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] {0,0});
 		
-		visited[x][y] = true;
-		if(x+1<=n && map[x+1][y] && !visited[x+1][y]) dfs(x+1, y, depth+1);
-		if(y+1<=m && map[x][y+1] && !visited[x][y+1]) dfs(x, y+1, depth+1);
-
-		if(x-1>0 && map[x-1][y] && !visited[x-1][y]) dfs(x-1, y, depth+1);
-		if(y-1>0 && map[x][y-1] && !visited[x][y-1]) dfs(x, y-1, depth+1);
-		visited[x][y] = false;
+		boolean[][] visited = new boolean[n][m];
+		visited[0][0] = true;
+		
+		while(!queue.isEmpty()) {
+			int[] v = queue.poll();
+			int x=v[0], y=v[1];
+			
+			for(int i=0; i<4; i++) {
+				int toX = x+dx[i], toY = y+dy[i];
+				
+				if(toX<0 || toY<0 || toX>=n || toY>=m) continue;
+				if(visited[toX][toY] || map[toX][toY]==0) continue;
+				
+				queue.add(new int[] {toX, toY});
+				map[toX][toY] = map[x][y]+1;
+				visited[toX][toY] = true;
+			}
+		}
 	}
 }
