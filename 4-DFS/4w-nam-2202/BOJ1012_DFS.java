a94a2b5f5725d0286ee6fdc2d89c67f424ecb819
nampongo <96946588+nampongo@users.noreply.github.com>
2022-02-11 21:28:28 +0900

    220211-FRI

--- 4-DFS/4w-nam-2202/BOJ1012_DFS.java
+++ 4-DFS/4w-nam-2202/BOJ1012_DFS.java
@@ -7,7 +7,6 @@ public class BOJ1012_DFS {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-<<<<<<< HEAD
 		int t = Integer.parseInt(br.readLine());
 		
 		while(t-->0) {
@@ -35,35 +34,8 @@ public class BOJ1012_DFS {
 					
 			System.out.println(cnt);
 		}
-=======
-		int t = Integer.parseInt(br.readLine()), cnt = 0;
-		
-		while(t-->0) {
-			StringTokenizer s = new StringTokenizer(br.readLine());
-			m = Integer.parseInt(s.nextToken());
-			n = Integer.parseInt(s.nextToken());
-			int k = Integer.parseInt(s.nextToken());
-			map = new boolean[n][m];
-			
-			while(k-->0) {
-				s = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(s.nextToken());
-				int y = Integer.parseInt(s.nextToken());
-				map[y][x] = true;
-			}
-		}
-		
-		for(int i=0; i<n; i++)
-			for(int j=0; j<m; j++) 
-				if(map[i][j] && !visited[i][j]) {
-					dfs(i, j);
-					cnt ++;
-				}
-		System.out.println(cnt);
->>>>>>> refs/remotes/origin/main
 	}
 	
-<<<<<<< HEAD
 	static void dfs(int y, int x) {
 		int[] dx = {1, -1, 0, 0}, dy = {0, 0, -1, 1};
 		visited[y][x] = true;
@@ -71,16 +43,6 @@ public class BOJ1012_DFS {
 		for(int i=0; i<4; i++) {
 			int tox = x+dx[i], toy = y+dy[i];
 			if(tox<0 || toy<0 || tox>=m || toy>=n) continue;
-=======
-	public static void dfs(int y, int x) {
-		int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
-		visited[y][x] = true;
-		
-		for(int i=0; i<4; i++) {
-			int tox = x+dx[i], toy = y+dy[i];
-			
-			if(tox<0|| toy<0|| tox>=m|| toy>=n) continue;
->>>>>>> refs/remotes/origin/main
 			if(!map[toy][tox] || visited[toy][tox]) continue;
 			
 			dfs(toy, tox);
--- 4-DFS/4w-nam-2202/BOJ2468.java
+++ 4-DFS/4w-nam-2202/BOJ2468.java
@@ -2,56 +2,46 @@ import java.util.*;
 import java.io.*;
 
 public class BOJ2468 {
-	static List<Integer> height;
 	static int[][] map;
 	static boolean[][] visited;
 	static int n;
  	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int max = Integer.MIN_VALUE, ex = Integer.MIN_VALUE;
 		n = Integer.parseInt(br.readLine());
 		
-		List<Integer> counted = new ArrayList<>();
-		height = new LinkedList<>();
 		map = new int[n][n];
 		visited = new boolean[n][n];
+		int max = Integer.MIN_VALUE;
 		
 		for(int i=0; i<n; i++) {
 			StringTokenizer s = new StringTokenizer(br.readLine());
 			for(int j=0; j<n; j++) {
 				map[i][j] = Integer.parseInt(s.nextToken());
-				if(map[i][j] > max) {
-					max = map[i][j];
-					height.add(max);
-				}
+				if(map[i][j] > max) max = map[i][j];
 			}
 		}
-		Collections.sort(height);
 		// height[0]값 기준으로 아래(flag:true)확인, 확인한 값은 counted에 저장
 		// 이전 확인 영역 수 ex에 저장, 다음 확인 시 ex값과 비교(ex값이 크면 break)
 		
-		int cur = height.get(0);
-		boolean flag = true;
+		int cur = max-1;
+		max = Integer.MIN_VALUE;
 		
-		while(true) {
-			counted.add(cur);
+		while(cur>=0) {
 			int cnt = 0;
 			
 			for(int i=0; i<n; i++)
 				for(int j=0; j<n; j++)
-					if(map[i][j]-cur>0) {
+					if(map[i][j]-cur>0 && !visited[i][j]) {
 						dfs(i, j, cur);
 						cnt++;
 					}
-			
-			if(ex>cnt) break;
-			else ex = cnt;
-			
-			if(flag) cur--;
-			//else cur++;
+
+			if(cnt > max) max = cnt;
+			visited = new boolean[n][n];
+			cur--;
 		}
-		System.out.println(ex);
+		System.out.println(max);
 	}
 	
 	public static void dfs(int y, int x, int cur) {
